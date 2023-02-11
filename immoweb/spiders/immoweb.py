c2af34ead4a6a1eded73eff062cce0bb5fca5255
Yogesh Sharma <yogesh.sharma.007321@gmail.com>
2023-02-11 15:45:39 -0500

    immoweb modified and updated

--- immoweb/spiders/immoweb.py
+++ immoweb/spiders/immoweb.py
@@ -1,6 +1,7 @@
 import scrapy
 import json
-
+from scrapy.crawler import CrawlerProcess
+import copy
 
 class ImmowebExtraction(scrapy.Spider):
     name = "immoweb_extraction_spider"
@@ -11,39 +12,76 @@ class ImmowebExtraction(scrapy.Spider):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.url = "https://www.immoweb.be/en/search-results?page={}"
+        self.page_url = "https://www.immoweb.be/en/search-results?page={}"
+        self.website_url = "https://immoweb.be"
 
 
     def start_requests(self):
+        yield scrapy.Request(url=self.website_url,
+                             callback=self.login)
 
+    def login(self, response):
+        xsrf = response.headers.getlist('Set-Cookie')[0].decode("utf-8").split(";")[0].split("=")[1]
+        immoweb_session = response.headers.getlist('Set-Cookie')[1].decode("utf-8").split(";")[0].split("=")[1]
+        url = 'https://www.immoweb.be/en/login'
+        payload = '{\"login-email\":\"thelouiswills4@gmail.com\",\"login-password\":\"Rock0004@\"}'
+        headers = {
+            'x-xsrf-token': xsrf.replace('%3D', '='),
+            'origin': 'https://www.immoweb.be',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
+            'cookie': f'XSRF-TOKEN={xsrf}; immoweb_session={immoweb_session}',
+            'content-type': 'application/json;charset=UTF-8'
+        }
+        yield scrapy.Request(url=url,
+                             method='POST',
+                             body=payload,
+                             headers=headers,
+                             callback=self.extract_pages
+                             )
+
+
+    def extract_pages(self, response):
+        print(response.json())
         for i in range(1, 334):
             yield scrapy.Request(
-                url=self.url.format(i),
+                url=self.page_url.format(i),
                 callback=self.extract_ids,
-                meta={
-                    'cookiejar': i
-                }
+                headers=response.headers
             )
 
 
+
     def extract_ids(self, response):
-        res_meta = response.meta
         try:
             res_data = response.json()
+            for ids in res_data['results']:
+                id = ids['id']
+
+                yield scrapy.Request(url='https://www.immoweb.be/en/statistics/summary/{}'.format(id),
+                                     meta=response.meta,
+                                     callback=self.extract_stats,
+                                     cb_kwargs={'id': id})
         except:
             self.log('Could not extract ids for  : {}'.format(response.request.url))
-            return
 
-        for ids in res_data['results']:
-            id = ids['id']
+
+
+    def extract_stats(self, response, id):
+        try:
+            res_data = response.json()
+            print(res_data)
             yield scrapy.Request(url='https://www.immoweb.be/en/classified/{}'.format(id),
-                                 meta=res_meta,
-                                 callback=self.extract_data)
+                                 meta=response.meta,
+                                 callback=self.extract_data,
+                                 cb_kwargs={'stats': res_data})
+        except:
+            self.log('Could not extract stats for id  : {}'.format(id))
 
 
-    def extract_data(self, response):
-        res_meta = response.meta
 
+    # data extaction code
+    def extract_data(self, response, stats):
 
         data = {}
         tr = response.xpath('//div[@class="text-block__body"]//table[@class="classified-table"]//tr')
@@ -93,7 +131,6 @@ class ImmowebExtraction(scrapy.Spider):
         if price:
             data['price'] = price
 
-
         # property details
         property_dict = {
             'prop_type': json_resp['property']['type'],
@@ -133,10 +170,23 @@ class ImmowebExtraction(scrapy.Spider):
             if agency_dict[i]:
                 data[i] = agency_dict[i]
 
+        for key, value in stats.items():
+            data['views'] = value['views']
+            data['bookmarks'] = value['bookmarks']
+
+        for i in final_images:
+            yield scrapy.Request(url=i,
+                                 cb_kwargs={'data': data}
+                                 )
 
 
-        yield data
+    def process_images(self, response, data):
+        pass
 
 
 
 
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(ImmowebExtraction)
+    process.start()
